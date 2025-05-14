9f9cc93afa72ee3f2088689bb2cb61f8d20c8d67
GFM-Augustinus-devotus <gfreitasmello67@gmail.com>
2025-05-14 18:57:35 -0300

    Now the data is correct in the pattern UTF-8 and now as querrys can be called a the answering of the questinos is working

--- llm.py
+++ llm.py
@@ -7,7 +7,8 @@ from datasets import load_dataset
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from langchain.schema import Document
 from dotenv import load_dotenv
 import os
@@ -96,52 +97,43 @@ for i in range(len(reader.pages)):
     print(RegimentoTextos)
 
 #Gerando arquivo do estatuto
-
-with open("Estatuto.txt", "w") as file:
+with open("Estatuto.txt", "w", encoding="utf-8") as file:
     file.write(EstatutoTitulo + "\n")
     file.write(EstatutoTexto + "\n")
 if file.closed:
     print("Arquivo criado com sucesso!")
 
 #Gerando arquivo do RGCG
-with open("RGCG.txt", "w") as file:
+with open("RGCG.txt", "w", encoding="utf-8") as file:
     file.write(RGCGtextos + "\n")
 if file.closed:
     print("Arquivo criado com sucesso!")
 
 #Gerando arquivo do Regimento
-with open("Regimento.txt", "w") as file:
+with open("Regimento.txt", "w", encoding="utf-8") as file:
     file.write(RegimentoTextos + "\n")
 if file.closed:
     print("Arquivo criado com sucesso!")
 
 #Gerando o DataFrame
-
-#Como são poucos arquivos eu posso gerar o DataFrame manualmente
-import os
-import pandas as pd
-
 data = {
-    'page_name': ['Estatuto.txt' ,'RGCG.txt', 'Regimento.txt'],  # Lista de nomes das páginas
-    'text': []  # Lista para armazenar os textos
+    'page_name': ['Estatuto.txt' ,'RGCG.txt', 'Regimento.txt'],
+    'text': []
 }
 
-
-with open("Estatuto.txt", "r") as file:
+with open("Estatuto.txt", "r", encoding="utf-8") as file:
     text1 = file.read()
     data['text'].append(text1)
 
-with open("RGCG.txt", "r") as file:
+with open("RGCG.txt", "r", encoding="utf-8") as file:
     text2 = file.read()
     data['text'].append(text2)
 
-with open("Regimento.txt", "r") as file:
+with open("Regimento.txt", "r", encoding="utf-8") as file:
     text3 = file.read()
     data['text'].append(text3)
 
-
 df = pd.DataFrame(data)
-
 print(df)
 
 #Gerando um arquivo tabular para o data frame
@@ -158,7 +150,17 @@ dataset = load_dataset(
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
-documents = [Document(page_content=text) for text in dataset['train']['text']]
+# documents = [Document(page_content=text) for text in dataset['train']['text']]
+
+print(dataset['train'].column_names)  # Adicione para depuração
+
+# Ajuste para garantir que está pegando o campo correto
+if 'text' in dataset['train'].column_names:
+    documents = [Document(page_content=text) for text in dataset['train']['text']]
+else:
+    # Tente pegar o primeiro campo disponível
+    first_col = dataset['train'].column_names[0]
+    documents = [Document(page_content=text) for text in dataset['train'][first_col]]
 
 texts = text_splitter.split_documents(documents)
 
@@ -179,18 +181,45 @@ Pergunta: {question}"""
 
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 chain_type_kwargs = {"prompt": PROMPT}
-qa = RetrievalQA.from_chain_type(llm="gpt-3.5-turbo", chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
+llm = ChatOpenAI(openai_api_key=key, model="gpt-3.5-turbo")
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
 
 #Gerando as perguntas relacionadas ao dataset selecionado
 
 query1 = "O que o Plano de Desenvolvimento Institucional da UFG apresenta?"
-resultado1 = qa.run(query1)
+resultado1 = qa.invoke({"query": query1})
+
 
 query2 = "Fale sobre a marca escolhida pela UFG como sua marca oficial, e por qual motivo ela escolheu esse tipo de marca?"
-resultado = qa.run(query2)
+resultado2 = qa.invoke({"query": query2})
 
 query3 = "O que a Portaria nº 2.569, de 26 de agosto de 2020 explica expecificamente, o que eu me devo atentar sobre ela?"
-resultado = qa.run(query3)
+resultado3 = qa.invoke({"query": query3})
 
 query4 = "Em que ano entrou em vigor o novo documento do Regimento da UFG e para que ele serve?"
-resultado = qa.run(query4)
+resultado4 = qa.invoke({"query": query4})
+
+#Mostrando as respostas geradas pelo modelo
+print("------------------------")
+print("------------------------")
+print(resultado1)
+print("------------------------")
+print("------------------------")
+
+print("------------------------")
+print("------------------------")
+print(resultado2)
+print("------------------------")
+print("------------------------")
+
+print("------------------------")
+print("------------------------")
+print(resultado3)
+print("------------------------")
+print("------------------------")
+
+print("------------------------")
+print("------------------------")
+print(resultado4)
+print("------------------------")
+print("------------------------")
