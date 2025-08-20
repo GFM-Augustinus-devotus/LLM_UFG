91aa8a3a59b0f6bc84a4a0b6c5b45af559a0103b
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2025-08-20 18:03:32 -0300

    Building the rag table for my incommes

--- Postgres/base.py
+++ Postgres/base.py
@@ -10,4 +10,19 @@ with Sessionlocal() as session:
 
     has_any = session.execute(select(RagResults.id).limit(1)).first() is not None
 
+    # if not has_any:
+    #     session.add(
+    #         pergunta = ""
+    #         resposta = ""
+    #         score1 = 0.0
+    #         score2 = 0.0
+    #         score3 = 0.0
+    #         score4 = 0.0
+    #         score5 = 0.0
+    #         score6 = 0.0
+    #         source = 0.0
+    #         page = 12
+    #     )
+    #     session.commit()
+
     #Fazer um ater table e colocar a coluna de perguntas para armazenar as perguntas
